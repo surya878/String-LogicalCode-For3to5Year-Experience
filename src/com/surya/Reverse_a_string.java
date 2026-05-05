54d6d88712b8ef2bc78ef3515b15f69e01ad4011
BootWithStream <paikaraysuryakanta1@gmail.com>
2026-05-05 19:51:46 +0530

    Execution flow

--- src/com/surya/Reverse_a_string.java
+++ src/com/surya/Reverse_a_string.java
@@ -19,3 +19,90 @@ public class Reverse_a_string {
 	}
 
 }
+
+/* ============================= Execution Flow ===============================
+
+ String s = "Surya";
+
+Indexes:
+
+Index	Character
+ 0	S
+ 1	u
+ 2	r
+ 3	y
+ 4	a
+
+s.length() = 5
+
+ 1. Correct Loop
+
+
+for (int i = s.length() - 1; i >= 0; i--)
+
+
+Actual values:
+for (int i = 4; i >= 0; i--)
+	
+Step-by-Step Flow
+
+Iteration 1
+
+i = 4
+
+condition
+
+4 >= 0   → true
+
+Charcter
+
+s.charAt(4) → 'a'
+
+Iteration 2
+
+i--
+
+Now:
+
+i = 3
+
+Character:
+
+s.charAt(3) → 'y'
+
+Iteration 3
+i = 2 → r
+Iteration 4
+i = 1 → u
+Iteration 5
+i = 0 → S
+
+Next
+i--
+
+Now:
+
+i = -1
+
+Condition:
+
+-1 >= 0 → false
+
+Loop stops.
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
